9adaa25a11499109a9fa1ab8cf5fa832d9fdfdd2
Hernandez Blanco, Marcos <MarcosHer@youforce.net>
2020-11-04 09:00:41 +0100

    Make it run from command line!

--- Program.cs
+++ Program.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using CommandLine;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Loader;
 
 namespace EfDbMigrator
 {
     class Program
     {
-        static void Main(string[] args)
+        public class Options
         {
-            Console.WriteLine("Hello World!");
+            [Option(longName: "publishFolder", Required = true, HelpText = "Folder containing the published DLLs.")]
+            public string PublishFolder { get; set; }
+
+            [Option(longName: "dbContextAssembly", Required = true, HelpText = "The assembly containing the DbContext to use.")]
+            public string DbContextAssembly { get; set; }
+
+            [Option(longName: "dbContextTypeName", Required = true, HelpText = "Name of the DbContext to use.")]
+            public string DbContextTypeName { get; set; }
+
+            [Option(longName: "connectionString", Required = true, HelpText = "The connection string.")]
+            public string ConnectionString { get; set; }
+        }
+
+        /// <summary>
+        /// Mains the specified publish folder.
+        /// </summary>
+        /// <param name="publishFolder">Folder containing the published DLLs.</param>
+        /// <param name="dnContextAssembly">The assembly containing the DbContext to use.</param>
+        /// <param name="dbContextTypeName">Name of the DbContext to use.</param>
+        /// <param name="connectionString">The connection string.</param>
+        public static void Main(string [] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    LoadAssemblies(o.PublishFolder);
+
+                    var dbContextAssemblyName = o.DbContextAssembly.Remove(o.DbContextAssembly.ToUpper().IndexOf(".DLL"));
+
+                    var contextDll = AssemblyLoadContext.Default.Assemblies.Single(a => a.GetName().Name == dbContextAssemblyName);
+                    var dbContextType = contextDll.GetTypes().Single(t => t.Name == o.DbContextTypeName);
+
+                    var DbContextOptionsBuilderType = typeof(DbContextOptionsBuilder<>).MakeGenericType(dbContextType);
+
+                    var optionsBuilder = Activator.CreateInstance(DbContextOptionsBuilderType) as DbContextOptionsBuilder;
+                    optionsBuilder.UseSqlServer(o.ConnectionString);
+
+                    var dbContext = Activator.CreateInstance(dbContextType, optionsBuilder.Options) as DbContext;
+
+                    dbContext.Database.Migrate();
+                });
+        }
+
+        private static void LoadAssemblies(string folder)
+        {
+            //var dlls = Directory.GetFiles($"{folder}{Path.DirectorySeparatorChar}*.dll");
+            var dlls = Directory.GetFiles(folder, "*.dll");
+
+            foreach (var dll in dlls)
+            {
+                AssemblyLoadContext.Default.LoadFromAssemblyPath(dll);
+            }
         }
     }
 }
